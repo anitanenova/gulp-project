ff03bf2cc7c7379ce438b7733f214efaed8bebfe
[anitanenova] <anita.nenova8@gmail.com>
2021-03-22 18:27:52 +0200

    fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -13,86 +13,90 @@ const babel = require('gulp-babel');
 const concat = require('gulp-concat'); // Concatenates files
 const imagemin = require('gulp-imagemin');
 
-  /* start task for scss */
-  gulp.task('sass', function(){
-    return gulp.src('./src/sass/master.scss')
-      .pipe(sass()) // Converts Sass to CSS with gulp-sass     
-          .pipe(sassLint())
-          .pipe(sassLint.format())
-          .pipe(sassLint.failOnError())
-          .pipe(sourcemaps.init())
-          .pipe(sass())
-          .pipe(postcss(autoprefixer())
-          .pipe(cssnano())
-          .pipe(rename({ extname: '.min.css' }))
-          .pipe(sourcemaps.write('./'))
-          .pipe(gulp.dest('dist/css')))
-  });
-  /* end task for scss */
 
-  /* start task for css min */
-  gulp.task('cssmin', function(){
-    return gulp.src('./dist/css/master.css')
-      .pipe(sourcemaps.init())
-      .pipe(cssnano())
-      .pipe(rename({ extname: '.min.css' }))
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('dist/css'))
-  });
-  /* end task for css min */
+const paths = {
+	source: {
+		scripts: "src/scripts/",
+		sass: "src/sass/",
+		images: "src/images/",
+	},
+	destination: {
+		scripts: "dist/scripts/",
+		css: "dist/css/",
+		images: "dist/images/",
+	},
+};
 
-  /* start task for scripts */
-  gulp.task('scripts', function(){
-    gulp.src(['./src/scripts/vendor/sample-library.js', './src/scripts/vendor/sample-library2.js'])
-      .pipe(babel({presets: ['@babel/preset-env'] })) 
-      .pipe(uglifyJS())
-      .pipe(concat('scripts.js'))
-      .pipe(gulp.dest('./src/scripts'))
-      .pipe(concat('bundle.js'))
-      .pipe(rename({ extname: '.min.js' }))
-      .pipe(gulp.dest('dist/scripts'))
-  });
-  /* ent task for scripts */
-
-  /* start task for images */
-
-  // !!! not working with gulp watch
-  /* gulp.task('images', function(){
-    return gulp.src('./src/images/*')
-      .pipe(imagemin())
-      .pipe(gulp.dest('dist/images'))
-  }); */
-
-  //working but only with the command 'gulp images'
-  function img() {
-    return gulp.src("./src/images/*")
-    .pipe(imagemin())
-    .pipe(gulp.dest("./dist/images"));
-  }
-
-  gulp.task('img', img);
-
-  gulp.task('watch', () => {
-    gulp.watch('./src/images/*', img);
-  });
-
-  gulp.task("images", gulp.series('img', 'watch'));
-
-  /* end task for images */
-
-
-  gulp.task('watch', function(){
-    browserSync.init({
-      server: {
-      baseDir: './'
-      },
-  });
-
-  gulp.watch('./src/sass/**/*.scss', gulp.series('sass')).on("change", browserSync.reload);
-  gulp.watch('./dist/css/master.css', gulp.series('cssmin')).on("change", browserSync.reload);
-  gulp.watch('./src/scripts/**/*.js', gulp.series('scripts')).on("change", browserSync.reload);
- // gulp.watch('./src/images/*', gulp.series('images')); 
+/* start task for scss */
+gulp.task('sass', function(){
+  return gulp.src(paths.source.sass + 'master.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass     
+        .pipe(sassLint())
+        .pipe(sassLint.format())
+        .pipe(sassLint.failOnError())
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(postcss(autoprefixer())
+        .pipe(cssnano())
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(paths.destination.css)))
 });
+/* end task for scss */
+
+/* start task for css min */
+gulp.task('cssmin', function(){
+  return gulp.src(paths.destination.css + 'master.css')
+    .pipe(sourcemaps.init())
+    .pipe(cssnano())
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.destination.css))
+});
+/* end task for css min */
+
+/* start task for scripts */
+gulp.task('scripts', function(){
+  return gulp.src([paths.source.scripts + '/vendor/sample-library.js', paths.source.scripts + 'vendor/sample-library2.js'])
+    .pipe(babel({presets: ['@babel/preset-env'] })) 
+    .pipe(concat('bundle.min.js'))
+    .pipe(uglifyJS())
+    .pipe(gulp.dest(paths.destination.scripts))
+});
+/* ent task for scripts */
+
+/* start task for images */
+
+function img() {
+  return gulp.src(paths.source.images + "*")
+  .pipe(imagemin())
+  .pipe(gulp.dest(paths.destination.images));
+}
+
+gulp.task('img', img);
+
+gulp.task('watch', () => {
+  gulp.watch('./src/images/*', img);
+});
+
+/* end task for images */
+
+
+gulp.task('watch', function(){
+  browserSync.init({
+    server: {
+    baseDir: './'
+    },
+});
+
+gulp.watch(paths.source.sass + '**/*.scss', gulp.series('sass')).on("change", browserSync.reload);
+gulp.watch(paths.destination.css + 'master.css', gulp.series('cssmin')).on("change", browserSync.reload);
+gulp.watch(paths.source.scripts + '**/*.js', gulp.series('scripts')).on("change", browserSync.reload); 
+});
+
+gulp.task("default", gulp.series('img', 'watch'));
+
+
 
  
  
